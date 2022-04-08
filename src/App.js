ef6685ce90b3b57336db1ae15d014b6034afcfb8
Billy <billy.m.nightingale@gmail.com>
2022-04-08 15:36:29 +0200

    Added props to student contact information, functioning

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
       <img className='wildlogo' src="https://www.wildcodeschool.com/assets/wildcodeschool-logo-meta-image-f6f2f7f52b82bfc419f031f6a989020a8b094d7a4e6676ab6f0dff0b0f470da9.png" alt="wildimage"></img>
       </header>
-      <Studentdetails />
+       <Studentdetails name="Billy Nightingale" location="Capbreton, France" about="Hello, I like to build things and am a web developer" funfact="I like Burittos" />
     </div>
   );
 }
--- src/components/students.js
+++ src/components/students.js
@@ -1,8 +1,16 @@
+//This is where all the student contact details will go and the cards
 
-const Studentdetails = () => (
-    <>
+const Studentdetails = (props) => (
+    <div>
         <h1>This is where student information goes</h1>
-    </>
+        <p>Name:{props.name}</p>
+        <p>Location:{props.location}</p>
+        <p>Fun Fact:{props.funfact}</p>
+        <p>About: {props.about}</p>
+        <a href="http://localhost:3000/">Add link here</a>
+        <a href="http://localhost:3000/">Add link here</a>
+        <a href="http://localhost:3000/">Add link here</a>
+    </div>
 );
 
 export default Studentdetails
